c6be9ecf2f99c6063f95745ae2700285a5ca1b0e
Juan Vidal <juanmvg2003@gmail.com>
2023-12-15 10:27:44 -0500

    variables

---
+++ basics.py
@@ -0,0 +1,11 @@
+# this is a comment
+# Rigth ways to create variables
+name  = "Juan Vidal" # correct
+_name = "Juan Vidal" # correct
+name001 = "Juan Vidal" # correct
+print(name)
+
+# wrong ways to create variables
+1name = "Juan Vidal" # incorrect
+first-name = "Juan Vidal" # incorrect
+name! = "Juan Vidal" # incorrect
