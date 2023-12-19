70325dbc62e7448ac2ca990b210d44bbc34d6547
Juan Vidal <juanmvg2003@gmail.com>
2023-12-18 19:13:26 -0500

    examples using simple data types

--- data-types/simpleDataTypes.py
+++ data-types/simpleDataTypes.py
@@ -2,3 +2,14 @@ text = "This is a text"
 int = 23
 double = 23.5
 boolean = True
+
+# exaples using data strings
+
+name = "Juan"
+lastName = "Vidal"
+fullName = name + " " + lastName
+concatenatedFullName = f"{name} {lastName}" # this converts everything into a string data
+
+# membership operators -- both returns a boolen value
+print('u' in concatenatedFullName) # verifing if there is a character or characters into a string
+print('u' not in concatenatedFullName) # verifing if there isn't a character or characters into a string
