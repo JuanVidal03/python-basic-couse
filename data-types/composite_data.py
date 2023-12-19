7fbd09f8214f54bdfbd0b03e8563d7ca61ca6b31
Juan Vidal <juanmvg2003@gmail.com>
2023-12-19 18:38:08 -0500

    composite data and examples

---
+++ data-types/composite_data.py
@@ -0,0 +1,14 @@
+array = [True, "Juan", "Vidal", 20] # it can be modify
+array[1] = "Mario" # OK
+
+tuple = (True, "Juan", "Vidal", 20) # it can not be modify, inmmutable
+#tuple[1] = "Mario" # Its not OK!!!!
+
+set = {True, "Juan", "Vidal", 20} # Sets in Python are unordered collections of unique elements | inmmutable
+# set[1] = "Mario" # Its not OK!!!!
+
+# this is a dictinonary
+dictinonary = {
+    "nombre": "Juan Vidal",
+    "edad": 20
+}
