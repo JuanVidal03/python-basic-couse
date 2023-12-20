6cbf9ea937cb2d106fa18fd6bd46f2b482e345c6
Juan Vidal <juanmvg2003@gmail.com>
2023-12-20 11:50:29 -0500

    inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,12 @@
+# get something from user
+# inout always return a string 
+name = input("Type your name: ")
+# show the value
+print(f"The username: {name}")
+
+
+# EXAMPLE USIGN NUMBERS
+number = input("Type a number: ")
+result = int(float(number)) * 2
+
+print(result)
