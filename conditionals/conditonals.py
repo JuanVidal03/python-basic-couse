cc059a28f52e4ea94610c8bf6c4d00ca82774a03
Juan Vidal <juanmvg2003@gmail.com>
2023-12-20 10:15:16 -0500

    conditionals

---
+++ conditionals/conditonals.py
@@ -0,0 +1,16 @@
+edad = 20
+
+if edad >= 18:
+    print('Es mayor de edad')
+else:
+    print('Es menor de edad')
+
+
+sueldo = 12000
+
+if sueldo <= 1000:
+    print('Ganas el minimo o menos')
+elif sueldo <= 10000:
+    print('Ganas bien')
+else:
+    print('Ganas muy bien')
