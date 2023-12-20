a302c75001f89d0a876a03c463feebaa2a17bd4c
Juan Vidal <juanmvg2003@gmail.com>
2023-12-19 19:07:41 -0500

    operator | arithmetic and comparison

---
+++ operators/arithmetic_operators.py
@@ -0,0 +1,21 @@
+# plus and minus (+ and -)
+plus = 2 + 2
+minus = 2 - 2
+
+# multiplication and division (* and *)
+multiplication = 2 * 2
+division = 2 / 2 # return a float data
+
+# exponent
+exponent = 2**5 # 2*2*2*2*2
+
+# down disivion 
+down_division = 25 // 2 # it returns a int value
+
+# module
+module = 25 % 2
+
+# to know the type of value of something
+name = "Juan Vidal"
+name_type = type(name) 
+print(name_type) #str it means string
---
+++ operators/comparison_operators.py
@@ -0,0 +1,13 @@
+# equals
+print(1 == 1)
+
+# differetn
+print(1 != 1)
+
+# less than or less or equals
+print(10 < 20)
+print(10 <= 20)
+
+# plus than and plus or equals  
+print(10 > 20)
+print(10 >= 20)
